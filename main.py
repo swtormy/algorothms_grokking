2cb8c461747bdede7c769e34d17aad650ba31f77
Konstantin Vasilev <swtormy@yahoo.com>
2023-01-04 12:58:25 +0300

    qs

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from search_algorithms.binary_search import binary_search
-from sort_algotithms.simple_sort import selection_sort
+from sort_algotithms.sorting_by_selection import selection_sort
 from sort_algotithms.quick_sort import quick_sort
 from utils import log_round
 from random import randint
+from time import time
+import sys
 
+
+print('Recursions before:', sys.getrecursionlimit())
+sys.setrecursionlimit(15000)
+print('Recursions after:', sys.getrecursionlimit())
 
 my_list = list(range(100))
 random_arr = list(range(6, 11)) + list(range(1, 6))
@@ -12,8 +18,14 @@ random_arr = list(range(6, 11)) + list(range(1, 6))
 # print(log_round(2, 1024))
 # #binary_search
 # print(binary_search(my_list, 2))
-# #simple_sort
 
+# #simple_sort
+# start = time()
 # print(selection_sort(random_arr))
+# end = time() - start
+# print('Simple sort:', end)
 #quicksort
+start = time()
 print(quick_sort(random_arr))
+end = time() - start
+print('Quick sort:', end)
--- sort_algotithms/quick_sort.py
+++ sort_algotithms/quick_sort.py
@@ -1,9 +1,14 @@
-def quick_sort(arr):
+from random import randint
+import sys
+
+def quick_sort(arr: list) -> list:
     if len(arr) < 2:
         return arr
     else:
-        pivot = arr[0]
-        less = [i for i in arr[1:] if i < pivot]
-        greather = [i for i in arr[1:] if i > pivot]
+        index = round(len(arr) / 2) #randint(0, len(arr)-1)
+        pivot = arr.pop(index) #arr[0]
+        less = [i for i in arr if i < pivot]
+        greather = [i for i in arr if i > pivot]
         return quick_sort(less) + [pivot] + quick_sort(greather)
+
 
