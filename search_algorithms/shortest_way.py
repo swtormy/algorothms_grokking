bd809b051ad42d97f9fdc436971f9da4d45fedd9
Konstantin Vasilev <swtormy@yahoo.com>
2023-01-06 15:18:17 +0300

    shortest_way

---
+++ search_algorithms/shortest_way.py
@@ -0,0 +1,32 @@
+from collections import deque
+
+graph = {}
+graph['you'] = ['alice','bob','claire']
+graph['bob'] = ['anuj','peggy']
+graph['alice'] = ['peggy']
+graph['claire'] = ['thom', 'jonny']
+graph['anuj'] = []
+graph['peggy'] = []
+graph['thom'] = []
+graph['jonny'] = []
+
+
+def person_is_seller(name: str) -> bool:
+    return name[-1] == 'm'
+
+def search_in_shortest_way(name: str) -> bool:
+    search_queue = deque()
+    search_queue += graph[name]
+    searched = []
+    while search_queue:
+        person = search_queue.popleft()
+        if not person in searched:
+            if person_is_seller(person):
+                print(person + " is a mango seller!")
+                return True
+            else: 
+                search_queue += graph[person]
+                searched.append(person)
+    return False
+
+search_in_shortest_way('you')
